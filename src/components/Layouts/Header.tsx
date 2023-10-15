bc53e9ed78bd2388550df91fdeac7e44cd1b942a
mdawlad29 <mdawladbdinfo@gmail.com>
2023-10-15 18:27:24 +0600

    Topbar

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,24 +1,21 @@
 "use client";
 import React from "react";
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import { Typography } from "antd";
 import { RiGraduationCapFill } from "react-icons/ri";
 import { MdFavoriteBorder } from "react-icons/md";
 import { BsCart2 } from "react-icons/bs";
 import { IoMdNotificationsOutline } from "react-icons/io";
-const { Title } = Typography;
 import { Select, Button, Space } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { Input } from "antd";
-
-const browserData = [
-  { label: "Browser", value: 1 },
-  { label: "Browser One", value: 2 },
-  { label: "Browser Two", value: 3 },
-];
+const TopBar = dynamic(() => import("@/components/Layouts/Topbar"), {
+  ssr: false,
+});
+const { Title } = Typography;
 
 const Header = () => {
-  const [selectType, setSelectType] = React.useState(browserData[0]);
   const [activeNav, setActiveNav] = React.useState<string>("");
 
   React.useEffect(() => {
@@ -42,68 +39,63 @@ const Header = () => {
 
   return (
     <>
+      <TopBar />
       <div
-        onCopy={(event) => {
-          event.preventDefault();
-        }}
+        className={`${activeNav} py-4 lg:px-14 md:px-6 sm:px-5 px-2 bg-neutral`}
       >
-        <div
-          className={`${activeNav} py-4 lg:px-14 md:px-6 sm:px-5 px-2 bg-neutral`}
-        >
-          <div className="flex justify-between items-center">
-            {/*--- Left Info ---*/}
-            <div className="flex items-center gap-10">
-              <Link href={"/"} className="flex items-center gap-3">
-                <RiGraduationCapFill className="inline w-7 h-7 text-primary" />
-                <Title level={3}>E-tutor</Title>
-              </Link>
-              {/*--- Search field ---*/}
-              <div className="lg:block hidden">
-                <div className="flex items-center gap-3">
-                  <Select
-                    style={{ width: 300, border: 0, borderRadius: 0 }}
-                    className={
-                      "border border-gray-200 focus:outline-none focus:border-primary rounded-none"
-                    }
-                    value={selectType}
-                    onChange={(e: any) => setSelectType(e.target?.value)}
-                    options={browserData.map((city) => ({
-                      label: city?.label,
-                      value: city?.value,
-                    }))}
-                  />
-                  <Input
-                    placeholder="What do you want to learn.."
-                    className={
-                      "border border-gray-200 focus:outline-none focus:border-primary rounded-none"
-                    }
-                    prefix={<SearchOutlined className="text-gray-500" />}
-                  />
-                </div>
+        <div className="flex justify-between flex-wrap items-center">
+          {/*--- Left Info ---*/}
+          <div className="flex items-center gap-10">
+            <Link href={"/"} className="flex items-center gap-3">
+              <RiGraduationCapFill className="inline w-7 h-7 text-primary" />
+              <Title level={3}>E-tutor</Title>
+            </Link>
+            {/*--- Search field ---*/}
+            <div className="lg:block hidden">
+              <div className="flex items-center gap-3">
+                <Select
+                  style={{ width: 300, border: 0, borderRadius: 0 }}
+                  className={
+                    "border border-gray-200 focus:outline-none focus:border-primary rounded-none"
+                  }
+                  defaultValue="Browser"
+                  options={[
+                    { label: "Browser", value: "browser" },
+                    { label: "Browser One", value: "browser-one" },
+                    { label: "Browser Two", value: "browser-two" },
+                  ]}
+                />
+                <Input
+                  placeholder="What do you want to learn.."
+                  className={
+                    "border border-gray-200 focus:outline-none focus:border-primary rounded-none"
+                  }
+                  prefix={<SearchOutlined className="text-gray-500" />}
+                />
               </div>
             </div>
-            {/*---- Right Info ---*/}
-            <div className="flex items-center gap-4">
-              <IoMdNotificationsOutline className="inline w-6 h-6 text-gray-500 cursor-pointer" />
-              <MdFavoriteBorder className="inline w-5 h-5 text-gray-500 cursor-pointer" />
-              <BsCart2 className="inline w-5 h-5 text-gray-500 cursor-pointer" />
-              <Space wrap>
-                <Button
-                  style={{
-                    backgroundColor: "transparent",
-                    borderRadius: 0,
-                  }}
-                  className={"bg-secondary text-primary border-none"}
-                >
-                  Create account
-                </Button>
-                <Button
-                  className={"bg-primary text-neutral border-none rounded-none"}
-                >
-                  Sign In
-                </Button>
-              </Space>
-            </div>
+          </div>
+          {/*---- Right Info ---*/}
+          <div className="flex items-center lg:gap-6 md:gap-4 gap-2">
+            <IoMdNotificationsOutline className="inline w-6 h-6 text-gray-500 cursor-pointer" />
+            <MdFavoriteBorder className="inline w-5 h-5 text-gray-500 cursor-pointer" />
+            <BsCart2 className="inline w-5 h-5 text-gray-500 cursor-pointer" />
+            <Space>
+              <Button
+                style={{
+                  backgroundColor: "transparent",
+                  borderRadius: 0,
+                }}
+                className={"bg-secondary text-primary border-none"}
+              >
+                Create account
+              </Button>
+              <Button
+                className={"bg-primary text-neutral border-none rounded-none"}
+              >
+                Sign In
+              </Button>
+            </Space>
           </div>
         </div>
       </div>
---
+++ src/components/Layouts/Topbar.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Select, Drawer, Space, Typography, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { RiGraduationCapFill } from "react-icons/ri";
+const { Title } = Typography;
+
+const links = [
+  { name: "home", link: "/" },
+  { name: "about", link: "/about" },
+  { name: "courses", link: "/courses" },
+  { name: "Contact", link: "/contact" },
+  { name: "become a instructor", link: "/instructor" },
+];
+
+const activeColor = "text-neutral";
+const inactiveColor = "text-gray-500";
+
+const TopBar = () => {
+  const [activeLink, setActiveLink] = React.useState<string>(links[0]?.name);
+  const [open, setOpen] = React.useState(false);
+
+  const setActive = (index: any) => {
+    setActiveLink(index);
+  };
+
+  return (
+    <>
+      <div className="bg-secondary-50 text-gray-500 py-3 lg:px-14 md:px-6 sm:px-5 px-2 ">
+        <div className="flex justify-between items-center gap-3">
+          {/*--- Left Header Item ---*/}
+          <div>
+            <AiOutlineMenu
+              onClick={() => setOpen(true)}
+              className="text-xl lg:hidden block cursor-pointer"
+            />
+            {/*--- Mobile Menu ----*/}
+            <Drawer
+              style={{ backgroundColor: "#1D2026" }}
+              placement={"left"}
+              closable={false}
+              onClose={() => setOpen(false)}
+              open={open}
+            >
+              <Link href={"/"} className="flex items-center gap-3">
+                <RiGraduationCapFill className="inline w-7 h-7 text-primary" />
+                <Title level={3} className="text-neutral">
+                  E-tutor
+                </Title>
+              </Link>
+              <ul className="capitalize">
+                {links.map((link) => (
+                  <li
+                    key={link?.name}
+                    className={`text-sm hover:text-neutral duration-200 ease-in-out my-3 ${
+                      activeLink === link?.name ? activeColor : inactiveColor
+                    }`}
+                    onClick={() => {
+                      setActive(link?.name);
+                      setOpen(false);
+                    }}
+                  >
+                    <Link href={link.link}>{link?.name}</Link>
+                  </li>
+                ))}
+              </ul>
+              <div className="space-y-4">
+                <Select
+                  style={{ width: "100%", border: 0, borderRadius: 0 }}
+                  className={
+                    "border border-gray-200 focus:outline-none focus:border-primary rounded-none"
+                  }
+                  defaultValue="Browser"
+                  options={[
+                    { label: "Browser", value: "browser" },
+                    { label: "Browser One", value: "browser-one" },
+                    { label: "Browser Two", value: "browser-two" },
+                  ]}
+                />
+                <Input
+                  placeholder="What do you want to learn.."
+                  className={
+                    "border border-gray-600 focus:outline-none focus:border-primary rounded-none bg-secondary-50"
+                  }
+                  prefix={<SearchOutlined className="text-gray-500" />}
+                />
+              </div>
+            </Drawer>
+            <div className="lg:block hidden">
+              <ul className="flex items-center gap-6 capitalize">
+                {links.map((link) => (
+                  <li
+                    key={link?.name}
+                    className={`text-sm hover:text-neutral duration-200 ease-in-out ${
+                      activeLink === link?.name ? activeColor : inactiveColor
+                    }`}
+                    onClick={() => setActive(link?.name)}
+                  >
+                    <Link href={link.link}>{link?.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/*---- Right Info ----*/}
+          <div>
+            <Space>
+              <Select
+                defaultValue="USD"
+                style={{ width: 80 }}
+                bordered={false}
+                options={[
+                  { value: "USD", label: "USD" },
+                  { value: "bangla", label: "Bangla" },
+                  { value: "hindi", label: "Hindi" },
+                ]}
+              />
+              <Select
+                defaultValue="USD"
+                style={{ width: 80, color: "#fff !important" }}
+                bordered={false}
+                options={[
+                  { value: "USD", label: "USD" },
+                  { value: "bangla", label: "Bangla" },
+                  { value: "hindi", label: "Hindi" },
+                ]}
+              />
+            </Space>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopBar;
